5840bc2aafe13d4557daa22e2518574f3bb02543
Rose-Zahra-Mohammadi <mohamadi.z.2016@gmail.com>
2022-06-10 18:11:34 -0400

    Initial

---
+++ user.py
@@ -0,0 +1,34 @@
+# declare a class and give it name User
+
+
+class User:
+# declaring a class attribute
+    bank_name = "First National Dojo"		
+    def __init__(self, name, email_address):
+        self.name = name
+        self.email = email_address
+        self.account_balance = 0
+    def make_deposit(self, amount):
+        self.account_balance += amount
+    
+    def make_withdrawal(self, amount):
+        self.account_balance -= amount
+    def display_user_balance(self):
+        print("User: " + self.name + ", Balance: $" + str(self.account_balance))
+    def transfer_money(self, other_user, amount):
+        self.account_balance -= amount
+        other_user.account_balance += amount
+
+guido = User("Gudio van Rossum", "guido@python.com")
+monty = User("Monty Python", "monty@python.com")
+rose = User("Rose Moha", "rose@python.com")
+# Accessing the instance's attributes
+# print(guido.name)  # output: Michael
+# print(monty.name)  # output: Michael
+monty.make_deposit(20)
+guido.make_deposit(10)
+rose.make_deposit(200)
+monty.display_user_balance()
+rose.transfer_money(monty, 15)
+rose.display_user_balance()
+monty.display_user_balance()
