4e1caf3e093fb9b246ad88d39a87a06eaab2f498
Rose-Zahra-Mohammadi <mohamadi.z.2016@gmail.com>
2022-06-10 18:21:48 -0400

    Using Chaining Method

--- user.py
+++ user.py
@@ -10,14 +10,18 @@ class User:
         self.account_balance = 0
     def make_deposit(self, amount):
         self.account_balance += amount
+        return self
     
     def make_withdrawal(self, amount):
         self.account_balance -= amount
+        return self
     def display_user_balance(self):
         print("User: " + self.name + ", Balance: $" + str(self.account_balance))
+        return self
     def transfer_money(self, other_user, amount):
         self.account_balance -= amount
         other_user.account_balance += amount
+        return self
 
 guido = User("Gudio van Rossum", "guido@python.com")
 monty = User("Monty Python", "monty@python.com")
@@ -29,6 +33,4 @@ monty.make_deposit(20)
 guido.make_deposit(10)
 rose.make_deposit(200)
 monty.display_user_balance()
-rose.transfer_money(monty, 15)
-rose.display_user_balance()
-monty.display_user_balance()
+rose.transfer_money(monty, 15).display_user_balance()
